eddca0efe2b40749039fbae2222699c4e4d4ed50
EZEN <EZEN@DESKTOP-AGK8123>
2021-06-11 20:59:55 +0900

    20210611 21:00 찐막

--- shy_engineering/src/main/java/repository/EmployeeRepository.java
+++ shy_engineering/src/main/java/repository/EmployeeRepository.java
@@ -12,6 +12,7 @@ public class EmployeeRepository {
 	SqlSession sqlSession;
 	private final String namespace = "mappers.employeeMapper";
 	private String statement;
+	
 	public void insertEmployee(EmployeeDTO dto) {
 		statement = namespace + ".employeeInsert";
 		sqlSession.insert(statement, dto);
@@ -26,7 +27,7 @@ public class EmployeeRepository {
 	}
 	public EmployeeDTO empInfo(String empNo) {
 		statement = namespace + ".empInfo";
-		return sqlSession.selectOne(statement, empNo);	// 해당 statement와 일치한는 empNo를 가지는 dto의 줄 반환
+		return sqlSession.selectOne(statement, empNo);
 	}
 	public void empUpdate(EmployeeDTO dto) {
 		statement = namespace + ".employeeUpdate";
